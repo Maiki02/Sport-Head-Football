7eba9a920103de5c9817fe25742ac72d9b56378c
Maiki02 <miqueasdavidgentile@gmail.com>
2025-04-05 14:23:21 -0300

    Pantalla de victoria

--- Assets/Scripts/UI/BackToMenu.cs
+++ Assets/Scripts/UI/BackToMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HowToPlay : MonoBehaviour
+public class BackToMenuController : MonoBehaviour
 {
    
    public void BackToMenu() {
